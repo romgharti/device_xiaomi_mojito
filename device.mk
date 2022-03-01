a3f8bb03f93ee23bda907c374c4886497b268ecc
romgharti <romgharti@gmail.com>
2022-03-01 17:20:08 +0545

    mojito: removed calculator

--- device.mk
+++ device.mk
@@ -137,8 +137,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
-    Snap \
-    ExactCalculator
+    Snap
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
