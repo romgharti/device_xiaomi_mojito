114bb0eb35969fa3a651786d402fef4eeb7d0051
romgharti <romgharti@gmail.com>
2022-06-07 19:50:54 +0545

    mojito: ShapeShiftOS Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_mojito.mk
+    $(LOCAL_DIR)/ssos_mojito.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_mojito-user \
-    aosp_mojito-userdebug \
-    aosp_mojito-eng
+    ssos_mojito-user \
+    ssos_mojito-userdebug \
+    ssos_mojito-eng
