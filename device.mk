0609c6d86dcbc16984051656200b2343e3a32616
Swapnil Vicky <swapnil.motoe@gmail.com>
2023-07-06 14:20:09 +0530

    mojito: Add maintainer string

--- device.mk
+++ device.mk
@@ -390,6 +390,9 @@ PRODUCT_PACKAGES += \
     TelephonyMojito \
     WifiMojito
 
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-bliss
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
