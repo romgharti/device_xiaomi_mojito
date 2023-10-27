a39fcc525fba4ccb3c82c1c597f13978473f4207
romgharti <romgharti@gmail.com>
2023-10-27 10:17:02 +0545

    mojito: Welcome Bliss ROM

--- bliss_mojito.mk
+++ bliss_mojito.mk
@@ -11,12 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common SuperiorOS ROM stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
-SUPERIOR_OFFICIAL := true
+# Inherit some common BlissRoms stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Product Specifics
-PRODUCT_NAME := superior_mojito
+PRODUCT_NAME := bliss_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
@@ -26,8 +25,5 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
-
-# Exclude AudioFx from build
-TARGET_EXCLUDES_AUDIOFX := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
