a17375681fa4e94a38b05e253ed1b0a627af7262
romgharti <romgharti@gmail.com>
2023-10-28 07:19:17 +0545

    mojito: Drop Fm radio for now

--- device.mk
+++ device.mk
@@ -236,9 +236,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
 # FM
-PRODUCT_PACKAGES += \
-    FM2 \
-    qcom.fmradio
+#PRODUCT_PACKAGES += \
+#    FM2 \
+#    qcom.fmradio
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
